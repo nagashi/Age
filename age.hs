4320144afb9864f894bda7a52130ab5230cf714e
Charles ORiley <ceoriley@gmail.com>
2025-07-07 12:47:14 -0500

    modified code to make recursive call

--- age.hs
+++ age.hs
@@ -1,11 +1,17 @@
 import Text.Read (readMaybe) -- Import readMaybe for safe parsing
 
-main :: IO () -- Declare the type of main as an IO action returning unit
-main = do -- Start a do-block for sequential IO actions
-  putStrLn "Please enter your birth year" -- Display a prompt to the user
-  year <- getLine -- Read a line from standard input into the variable 'year'
+getInput :: IO Int  -- Function getInput is of type IO that returns type Int
+getInput = do
+  -- Start a do-block for sequential IO actions
+  putStrLn "Please enter your birth year"
+  inputStr <- getLine
+  case readMaybe inputStr :: Maybe Int of  -- Attempt to read an Int, resulting in Maybe Int.
+    Nothing -> do
+      putStrLn "Invalid input.  Please try again."
+      getInput  -- Recursive call to restart the input process.
+    Just val -> return val -- Return the valid value within the IO monad.
 
-  case readMaybe year :: Maybe Int of -- Safely attempt to parse the input 'year' as an integer
-    Nothing -> putStrLn "You provided an invalid year" -- Handle the case where parsing fails
-    Just birthYear -> -- If parsing is successful, bind the integer to 'birthYear'
-      putStrLn $ "In 2025, you will be: " ++ show (2025 - birthYear) -- Calculate and print the age
+main :: IO () -- Declare the type of main as an IO action returning unit
+main = do
+  birthYear <- getInput --  Call the recursive function.
+  putStrLn $ "In 2025, you will be: " ++ show (2025 - birthYear) -- Calculate and print the age
