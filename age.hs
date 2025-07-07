4d1adcee894702fac326b731ec6030bc1839d987
Charles ORiley <ceoriley@gmail.com>
2025-07-07 14:13:07 -0500

    allow user to shut down gracefully

--- age.hs
+++ age.hs
@@ -1,15 +1,26 @@
-import Text.Read (readMaybe) -- Import readMaybe for safe parsing
+-- Import readMaybe for safe parsing
 
-getInput :: IO Int  -- Function getInput is of type IO that returns type Int
+import Data.Char (toLower)
+import System.Exit
+import Text.Read (readMaybe)
+
+stringToLower :: String -> String
+stringToLower str = map toLower str
+
+getInput :: IO Int -- Function getInput is of type IO that returns type Int
 getInput = do
   -- Start a do-block for sequential IO actions
-  putStrLn "Please enter your birth year"
+  putStrLn "Please enter your birth year or\ntype QUIT to exit"
   inputStr <- getLine
-  case readMaybe inputStr :: Maybe Int of  -- Attempt to read an Int, resulting in Maybe Int.
-    Nothing -> do
-      putStrLn "Invalid input.  Please try again."
-      getInput  -- Recursive call to restart the input process.
-    Just val -> return val -- Return the valid value within the IO monad.
+
+  let lowerCaseString = stringToLower inputStr
+  if lowerCaseString == "quit"
+    then putStrLn "Exiting..." >> exitSuccess
+    else case readMaybe inputStr :: Maybe Int of -- Attempt to read an Int, resulting in Maybe Int.
+      Nothing -> do
+        putStrLn "Invalid input.  Please try again."
+        getInput -- Recursive call to restart the input process.
+      Just val -> return val -- Return the valid value within the IO monad.
 
 main :: IO () -- Declare the type of main as an IO action returning unit
 main = do
